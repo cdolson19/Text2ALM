4e544b0cecc17cf881d60a88eeef20d6501ebd1d
Craig Olson <craig.olson19@gmail.com>
2020-03-05 23:13:38 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 """
 A config python file to store run-time variables.
 """
-
+# System Setup:
+# Part 1:
+# This host address must be replaced with your IP address or host name.
 CORE_NLP_HOST = 'http://leelavati.ist.unomaha.edu'
+
+# Part 2:
+# This port must match the port used for the Stanford CoreNLP system.
 CORE_NLP_PORT = 9001
-LTH_HOST = 'http://leelavati.ist.unomaha.edu:8071/parse'
+
+# Part 3:
+# This port must match the port used for the LTH system.
+LTH_PORT = 8071
+
+# Part 4:
+# This host name must be replaced with your IP address or host name.
+LTH_HOST = f'http://leelavati.ist.unomaha.edu:{LTH_PORT}/parse'
